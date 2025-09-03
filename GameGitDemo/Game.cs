c74b7383d896da9a297639a818c9aa122c96e4af
iaravalentinaricca <iaravalentinaricca@gmail.com>
2025-09-03 15:59:06 +0100

    Updated Previous Changes

--- GameGitDemo/Game.cs
+++ GameGitDemo/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
@@ -12,18 +13,29 @@ namespace GameGitDemo
         public void Run()
         {
             WriteLine("   ___    _     _         ___             \r\n  /___\\__| | __| |___    /___\\_ __  _ __  \r\n //  // _` |/ _` / __|  //  // '_ \\| '_ \\ \r\n/ \\_// (_| | (_| \\__ \\ / \\_//| | | | | | |\r\n\\___/ \\__,_|\\__,_|___/ \\___/ |_| |_|_| |_|\r\n                                          ");
-            WriteLine("*** Welcome to the Game: " +
-                "Odds On! ***");
+            WriteLine("*** Welcome to the Game ***");
 
-
-            Write("\nWhat is your name? ");
+            Write("\nPlayer 1, what is your name? ");
             string name = ReadLine();
-            Player CurrentPlayer = new Player(name);
-            WriteLine($"Let's get started {CurrentPlayer.Name}!");
+            Player player = new Player(name);
+            Clear();
 
+            
+        }
+
+        public void GameStart()
+        {
+           
+
+        }
+
+        public void EndofGame()
+        { 
             WriteLine("\nPress any key to exit...");
             ReadKey(true);
         }
 
     }
+    
+    
 }
